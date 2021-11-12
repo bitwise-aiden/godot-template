07f892436954164451f062b761ca82bf1704050a
velopman <74576289+velopman@users.noreply.github.com>
2021-11-12 02:35:33 -0500

    Update ridiculous_coding to enable in current script on start

--- addons/ridiculous_coding/plugin.gd
+++ addons/ridiculous_coding/plugin.gd
@@ -18,6 +18,8 @@ func _enter_tree():
 	var script_editor = editor.get_script_editor()
 	script_editor.connect("editor_script_changed", self, "editor_script_changed")
 
+	get_all_text_editors(script_editor)
+
 
 func _exit_tree():
 	pass
@@ -28,14 +30,14 @@ func get_all_text_editors(parent : Node):
 	for child in parent.get_children():
 		if child.get_child_count():
 			get_all_text_editors(child)
-			
+
 		if child is TextEdit:
 			editors[child] = { "text": child.text, "line": child.cursor_get_line() }
-			
+
 			if child.is_connected("cursor_changed", self, "cursor_changed"):
 				child.disconnect("cursor_changed", self, "cursor_changed")
 			child.connect("cursor_changed", self, "cursor_changed", [child])
-				
+
 			if child.is_connected("text_changed", self, "text_changed"):
 				child.disconnect("text_changed", self, "text_changed")
 			child.connect("text_changed", self, "text_changed", [child])
@@ -50,19 +52,19 @@ func gui_input(event):
 	if event is InputEventKey and event.pressed:
 		event = event as InputEventKey
 		last_key = OS.get_scancode_string(event.get_scancode_with_modifiers())
-		
+
 
 func editor_script_changed(script):
 	var editor = get_editor_interface()
 	var script_editor = editor.get_script_editor()
-	
+
 	editors.clear()
 	get_all_text_editors(script_editor)
 
 
 func _process(delta):
 	var editor = get_editor_interface()
-	
+
 	if shake > 0:
 		shake -= delta
 		editor.get_base_control().rect_position = Vector2(rand_range(-shake_intensity,shake_intensity), rand_range(-shake_intensity,shake_intensity))
@@ -77,46 +79,46 @@ func _process(delta):
 func shake(duration, intensity):
 	if shake > 0:
 		return
-		
+
 	shake = duration
 	shake_intensity = intensity
-	
-	
+
+
 func cursor_changed(textedit):
 	var editor = get_editor_interface()
-	
+
 	if not editors.has(textedit):
 		# For some reason the editor instances all change
 		# when the file is saved so you need to reload them
 		editors.clear()
 		get_all_text_editors(editor.get_script_editor())
-		
+
 	editors[textedit]["line"] = textedit.cursor_get_line()
 
 
 func text_changed(textedit : TextEdit):
 	var editor = get_editor_interface()
 	var settings = editor.get_editor_settings()
-	
+
 	if not editors.has(textedit):
 		# For some reason the editor instances all change
 		# when the file is saved so you need to reload them
 		editors.clear()
 		get_all_text_editors(editor.get_script_editor())
-		
+
 	# Get line and character count
 	var line = textedit.cursor_get_line()
 	var column = textedit.cursor_get_column()
-	
+
 	# Compensate for tab size
 	var tab_size = settings.get_setting("text_editor/indent/size")
 	var line_text = textedit.get_line(line).substr(0,column)
 	column += line_text.count("\t") * (tab_size - 1)
-	
+
 	# Compensate for scroll
 	var vscroll = textedit.scroll_vertical
 	var hscroll = textedit.scroll_horizontal
-	
+
 	# When you are scrolled to the bottom of a file
 	# and you delete some lines from the bottom using Ctrl+X
 	# then the vscroll can go down without changing the visible
@@ -127,16 +129,16 @@ func text_changed(textedit : TextEdit):
 	# lines to remove the gap. It changes the editor behavior
 	# slightly for a better result.
 	textedit.scroll_vertical = vscroll
-	
+
 	# Compensate for line spacing
 	var line_spacing = settings.get_setting("text_editor/theme/line_spacing")
-	
+
 	# Load editor font
 	var font : DynamicFont = DynamicFont.new()
 	font.font_data = load(settings.get_setting("interface/editor/code_font"))
 	font.size = settings.get_setting("interface/editor/code_font_size")
 	var fontsize = font.get_string_size(" ")
-	
+
 	# Compute caret position
 	var pos = Vector2()
 	pos.x = (column) * (fontsize.x) - hscroll + 100
@@ -146,14 +148,14 @@ func text_changed(textedit : TextEdit):
 		# Deleting
 		if timer > 0.1 and len(textedit.text) < len(editors[textedit]["text"]):
 			timer = 0.0
-			
+
 			# Draw the thing
 			var thing = Boom.instance()
 			thing.position = pos
 			thing.destroy = true
 			thing.last_key = last_key
 			textedit.add_child(thing)
-			
+
 			# Shake
 			shake(0.2, 10)
 
@@ -169,10 +171,10 @@ func text_changed(textedit : TextEdit):
 			thing.destroy = true
 			thing.last_key = last_key
 			textedit.add_child(thing)
-			
+
 			# Shake
 			shake(0.05, 5)
-			
+
 		# Newline
 		if textedit.cursor_get_line() != editors[textedit]["line"]:
 			# Draw the thing
@@ -180,7 +182,7 @@ func text_changed(textedit : TextEdit):
 			thing.position = pos
 			thing.destroy = true
 			textedit.add_child(thing)
-			
+
 			# Shake
 			shake(0.05, 5)
 
